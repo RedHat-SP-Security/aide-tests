b97f1d8f804023bc1af29677fa41cc97a76ebb9d
Marek Safarik <msafarik@msafarik-thinkpadp1gen4i.tpbc.csb>
2025-03-04 15:47:13 +0100

    Simple main fmf

--- Sanity/aide-running-as-user/runtest.sh
+++ Sanity/aide-running-as-user/runtest.sh
@@ -41,7 +41,7 @@ rlJournalStart
     rlPhaseEnd
 
     rlPhaseStartTest "Checking axioms"
-        rlRun "touch file.txt" 0 "Creating simple file"
+        rlRun "touch file.txt" 0 "Creating simple file" 
         rlRun "echo 'Random text' > file.txt" 0  "Filling the file with text"
        rlAssertGrep "Random text" "$TmpDir/file.txt"
     rlPhaseEnd
