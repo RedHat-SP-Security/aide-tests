95a332bbf1adee5e9868f88e42b428ce20832750
Marek Safarik <msafarik@msafarik-thinkpadp1gen4i.tpbc.csb>
2025-03-11 17:03:54 +0100

    Fully functional test

--- Sanity/aide-running-as-user/runtest.sh
+++ Sanity/aide-running-as-user/runtest.sh
@@ -43,43 +43,41 @@ rlJournalStart
         rlAssertRpm $PACKAGE || rlDie 'cannot continue'
         rlRun "mkdir -p $TEST_DIR/{,data,db,log}"
         rlRun "mv $AIDE_CONFIG $TEST_DIR/aide.conf"
-        rlRun "touch $TEST_DIR/data/empty.txt"
-        rlRun "echo 'a' > $TEST_DIR/data/a.txt"
-        rlRun "echo 'b' > $TEST_DIR/data/b.txt"
         rlRun "chmod a=rwx $TEST_DIR/*"
+        rlRun "chmod a=rwx $testUserHomeDir/*"
 
+        rlRun "testUserSetup"
         echo 'int main(void) { return 0; }' > $TEST_DIR/main.c
         # rlRun "chmod a+rw $TEST_DIR/main.c" 0
-        exe1="${testUserHomeDir}/exe1"
-        exe2="${testUserHomeDir}/exe2"
+        # exe1="${testUserHomeDir}/exe1"
+        # exe2="${testUserHomeDir}/exe2"
+        exe1="${TEST_DIR}/data/exe1"
+        exe2="${TEST_DIR}/data/exe2"
+        rlRun "su -c 'aide -i -c $TEST_DIR/aide.conf' - $testUser" 0 "Initializing AIDE database as $testUser"
+        rlRun "mv -f $TEST_DIR/db/aide.db.out.gz $TEST_DIR/db/aide.db.gz"
         rlRun "gcc $TEST_DIR/main.c -o $exe1" 0 "Creating binary $exe1"
         rlRun "gcc $TEST_DIR/main.c -g -o $exe2" 0 "Creating binary $exe2"
-        rlRun "chmod a+rx $exe1 $exe2 ${testUserHomeDir}"
-        rlRun "testUserSetup"
-
-        rlRun "su -c 'aide -i -c $TEST_DIR/aide.conf' - $testUser" 0 "Initializing AIDE database as $testUser"
+        rlRun "chmod a=rx $exe1 $exe2 ${testUserHomeDir}"
 
     rlPhaseEnd
 
 
     rlPhaseStartTest "Testing running as a user"
-        # rlRun "su -c 'gcc $TEST_DIR/main.c -o $exe1' - $testUser" 0 "Creating binary $exe1 as $testUser"
-        # rlRun "su -c 'gcc $TEST_DIR/main.c -g -o $exe2' - $testUser" 0 "Creating binary $exe2 as $testUser"
-
-
-
+        rlRun -s "su -c 'aide --check -c $TEST_DIR/aide.conf' - $testUser" 1 "Checking changes as $testUser"
+        rlRun "su -c 'aide --update -c $TEST_DIR/aide.conf' - $testUser" 1 "Updating AIDE database as $testUser"
+        rlRun "mv -f $TEST_DIR/db/aide.db.out.gz $TEST_DIR/db/aide.db.gz"
 
 
         rlRun "su -c '$exe1' - $testUser" 0 "cache trusted binary $exe1"
         rlRun "su -c '$exe2' - $testUser" 0 "check untrusted binary $exe2"
-        rlRun "touch file.txt" 0 "Creating simple file" 
-        rlRun "echo 'Random text' > file.txt" 0  "Filling the file with text"
-        rlAssertGrep "Random text" "file.txt"
+
+        rlRun -s "su -c 'aide --check -c $TEST_DIR/aide.conf' - $testUser" 0 "Checking changes as $testUser"
     rlPhaseEnd
 
     rlPhaseStartCleanup
+        rlRun "rm -r $testUserHomeDir" 0 "Removing testing directory"
         rlRun "testUserCleanup"
-        rlRun "rm -r $TEST_DIR" 0 "Removing tmp directory"
+        rlRun "rm -r $TEST_DIR" 0 "Removing testing directory"
     rlPhaseEnd
 rlJournalPrintText
 rlJournalEnd
